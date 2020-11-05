8971ca12d899eeb78259c299b9001f5e87e153db
ulises <udorantes@trigarante.com>
2020-11-04 18:02:09 -0600

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AceptoComponent } from './components/acepto/acepto.component';
+import { PropuestaComponent } from './components/propuesta/propuesta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'propuesta', component: PropuestaComponent},
+  { path: 'acepto', component: AceptoComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'propuesta' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AceptoComponent } from './components/acepto/acepto.component';
+import { PropuestaComponent } from './components/propuesta/propuesta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AceptoComponent,
+    PropuestaComponent
   ],
   imports: [
     BrowserModule,
